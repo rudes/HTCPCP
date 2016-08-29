71e24404c81ffe1b76fefcbf5fc1413b0636772d
rudes <jacobriddle94@gmail.com>
2016-08-28 21:52:58 -0400

    Adding Errors and Extending Headers
    - Added two error responses
    - Extending headernames

--- Network/HTCPCP/Pot.hs
+++ Network/HTCPCP/Pot.hs
@@ -4,8 +4,8 @@ import Network.URI
 
 data PotRequestMethod = BREW | GET | PROPFIND | WHEN
 data PotHeader = PotHeader PotHeaderName String
-data PotHeaderName =
-    SAFE
+data PotErrorResponse = NA | TPOT
+data PotHeaderName = SAFE | ACCEPT
 data PotRequest a =
     Request { prqURI :: URI
             , prqMethod :: PotRequestMethod
@@ -15,7 +15,13 @@ data PotRequest a =
 instance Show PotRequestMethod where
     show x =
         case x of
-            BREW       ->   "BREW"
+            BREW       ->   "BREW" -- Post
             GET        ->   "GET"
             PROPFIND   ->   "PROPFIND"
             WHEN       ->   "WHEN"
+
+instance Show PotErrorResponse where
+    show x =
+        case x of
+          NA -> "406 Not Acceptable"
+          TPOT -> "418 I'm a Teapot"
