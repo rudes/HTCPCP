9485131c08d5c9fe6248311acac16e2a787d49a3
rudes <jacobriddle94@gmail.com>
2016-08-29 00:20:12 -0400

    Extending PotHeaders and adding Instances of Show
    - Added text for all additions
    - Added URI format

--- Network/HTCPCP/Pot.hs
+++ Network/HTCPCP/Pot.hs
@@ -2,10 +2,26 @@ module Network.HTCPCP.Pot where
 
 import Network.URI
 
-data PotRequestMethod = BREW | GET | PROPFIND | WHEN
+data PotRequestMethod = BREW | GET | PROPFIND | WHEN | Custom String
 data PotHeader = PotHeader PotHeaderName String
 data PotErrorResponse = NA | TPOT
-data PotHeaderName = SAFE | ACCEPT
+data PotSafeHeader = YES | NO | CONDITONAL Bool
+data PotMilk = CREAM | HALFANDHALF | WHOLEMILK | PARTSKIM | SKIM | NONDAIRY
+data PotSyrup = VANILLA | ALMOND | RASPBERRY | CHOCOLATE
+data PotAlcohol = WHISKY | RUM | KAHLUA | AQUAVIT
+data PotAddType = MILK PotMilk
+                | SYRUP PotSyrup
+                | ALCOHOL PotAlcohol
+                | CustomAddition String
+data PotHeaderName = SAFE | ACCEPT PotAddType
+-- potUriScheme://potUriHost/potDesignator#potAdditions
+-- coffee://host-address/pot-1#CREAM
+data PotURI = PotURI
+    { potUriScheme :: String
+    , potUriHost :: String
+    , potDesignator :: String
+    , potAdditions :: PotAddType
+    }
 data PotRequest a =
     Request { prqURI :: URI
             , prqMethod :: PotRequestMethod
@@ -25,3 +41,44 @@ instance Show PotErrorResponse where
         case x of
           NA -> "406 Not Acceptable"
           TPOT -> "418 I'm a Teapot"
+
+instance Show PotHeaderName where
+    show x =
+        case x of
+          SAFE -> "Safe"
+          ACCEPT t -> show t
+
+instance Show PotAddType where
+    show x =
+        case x of
+          MILK m -> show m
+          SYRUP s -> show s
+          ALCOHOL a -> show a
+          CustomAddition s -> s
+
+instance Show PotMilk where
+    show x =
+        case x of
+          CREAM -> "CREAM"
+          HALFANDHALF -> "HALF-AND-HALF"
+          WHOLEMILK -> "WHOLE MILK"
+          PARTSKIM -> "PART SKIM"
+          SKIM -> "SKIM"
+          NONDAIRY -> "NON-DAIRY"
+
+instance Show PotSyrup where
+    show x =
+        case x of
+          VANILLA -> "VANILLA"
+          ALMOND -> "ALMOND"
+          RASPBERRY -> "RASPBERRY"
+          CHOCOLATE -> "CHOCOLATE"
+
+instance Show PotAlcohol where
+    show x =
+        case x of
+          WHISKY -> "Whisky"
+          RUM -> "Rum"
+          KAHLUA -> "Kahlua"
+          AQUAVIT -> "Aquavit"
+
