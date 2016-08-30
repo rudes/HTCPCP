2e12ad9afda5e69995d86d0e2c581daec143ca6d
rudes <jacobriddle94@gmail.com>
2016-08-29 21:30:37 -0400

    Adding PotRequest UnitTest

--- test/Network/HTCPCP/PotSpec.hs
+++ test/Network/HTCPCP/PotSpec.hs
@@ -10,8 +10,16 @@ spec :: Spec
 spec = do
     describe "PotURI" $ do
         it "builds a Coffee URI" $ do
-            show PotURI { potUriScheme = "coffee"
-                   , potUriHost = "host"
-                   , potDesignator = 2
-                        , potAdditions = MILK WHOLEMILK
-                    } `shouldBe` ("coffee://host/pot-2#WHOLE-MILK" :: String)
+            show PotURI { potUriScheme = "coffee", potUriHost = "host"
+                        , potDesignator = 2, potAdditions = MILK WHOLEMILK
+                } `shouldBe` ("coffee://host/pot-2#WHOLE-MILK" :: String)
+    describe "PotRequest" $ do
+        it "builds a Coffee Request" $ do
+            show PotRequest { prqURI = PotURI { potUriScheme = "coffee"
+                                              , potUriHost = "host"
+                                              , potDesignator = 2
+                                              , potAdditions = MILK WHOLEMILK }
+                            , prqMethod = GET
+                            , prqHeaders = [(PotHeader SAFE "yes")]
+                            , prqBody = "test" }
+                            `shouldBe` ("GET coffee://host/pot-2#WHOLE-MILK \r\nSafe:yes\r\ntest" :: String)
