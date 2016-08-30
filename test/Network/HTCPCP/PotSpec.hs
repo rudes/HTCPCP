04f528b781048e110490f7a643d29b00ab7814a5
rudes <jacobriddle94@gmail.com>
2016-08-29 22:26:51 -0400

    Adding Error Test
    - Removing reduntant DO
    - Updating for new show syntax

--- test/Network/HTCPCP/PotSpec.hs
+++ test/Network/HTCPCP/PotSpec.hs
@@ -8,18 +8,22 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "PotURI" $ do
-        it "builds a Coffee URI" $ do
+    describe "PotURI" $
+        it "builds a Coffee URI" $
             show PotURI { potUriScheme = "coffee", potUriHost = "host"
                         , potDesignator = 2, potAdditions = MILK WHOLEMILK
                 } `shouldBe` ("coffee://host/pot-2#WHOLE-MILK" :: String)
-    describe "PotRequest" $ do
-        it "builds a Coffee Request" $ do
+    describe "PotRequest" $
+        it "builds a Coffee Request" $
             show PotRequest { prqURI = PotURI { potUriScheme = "coffee"
                                               , potUriHost = "host"
                                               , potDesignator = 2
                                               , potAdditions = MILK WHOLEMILK }
                             , prqMethod = GET
-                            , prqHeaders = [(PotHeader SAFE "yes")]
+                            , prqHeaders = [PotHeader SAFE "yes"
+                                           , PotHeader ACCEPT "WHOLE-MILK"]
                             , prqBody = "test" }
-                            `shouldBe` ("GET coffee://host/pot-2#WHOLE-MILK \r\nSafe:yes\r\ntest" :: String)
+                            `shouldBe` ("GET coffee://host/pot-2#WHOLE-MILK \r\nSafe:yes;Accept-Additions:WHOLE-MILK;\r\ntest" :: String)
+    describe "PotErrorResponse" $
+        it "builds a Coffee Error Response" $
+            show NA `shouldBe` ("406 Not Acceptable" :: String)
